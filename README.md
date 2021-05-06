86a57e01240c4707ae3c9fbfb637682257d8bf1a
James Njuguna <james@arifu.com>
2021-05-06 11:17:26 +0300

    --adding teh first commit with all the models in place --for User, DrugCategory, Drug and Dosage

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# medicine_dose_tracker
-Medicine dose tracker web app  A web application that allows patients to get notifications about their prescriptions.
+####
+##Medicine Dose Tracker Web App 
+####
+A web application that allows patients to get notifications about their prescriptions.
+The App allows user can sign up for an account. Once logged in, the user is presented with a simple form to add medicine name, dosages, and frequency. After adding those details, the user can view, edit, or delete this information.
---
+++ app/models.py
@@ -0,0 +1,96 @@
+import jwt
+from datetime import datetime, time
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app import app, db, login
+
+
+class TimestampMixin(object):
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
+
+
+class User(TimestampMixin, UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    first_name = db.Column(db.String(30))
+    last_name = db.Column(db.String(30))
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
+
+
+class DrugCategory(TimestampMixin, db.Model):
+    """
+    paracetamol
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+
+    def __repr__(self):
+        return f'DrugCategory {self.name}'
+
+
+class Drug(TimestampMixin, db.Model):
+    """
+    drug
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    book_type = db.Column(db.Integer, db.ForeignKey('book_type.id'))
+    minimum_charge = db.Column(db.Numeric(5, 2))
+    no_of_days = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'Drug {self.username}'
+
+
+class Dosage(TimestampMixin, db.Model):
+    """
+    dosage
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    drug_id = db.Column(db.Integer, db.ForeignKey('drug.id'))
+    no_of_times = db.Column(db.Integer)
+    frequency = db.Column(db.Integer)
+
+    def __repr__(self):
+        user = User.query.filter_by(id=self.user_id).first()
+        drug = Drug.query.filter_by(id=self.drug_id).first()
+        return f'{drug.name} has been prescribed to {user.first_name} {user.last_name}'
+
+    def get_interval(self):
+        user = User.query.filter_by(id=self.user_id).first()
+        drug = Drug.query.filter_by(id=self.drug_id).first()
+        return f'{user.get_full_name()} take {drug.name}  {drug.no_of_times} * {drug.frequency} times a day'
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
